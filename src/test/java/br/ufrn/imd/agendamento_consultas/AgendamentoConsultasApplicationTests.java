0f4c5b3876b6bfec603d97a6c146614e5316758f
ewerton.carvalho.096 <ewerton.carvalho.096@imd.ufrn.br>
2024-12-05 15:08:22 -0300

    Primeiro commit

---
+++ src/test/java/br/ufrn/imd/agendamento_consultas/AgendamentoConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.agendamento_consultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoConsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/lp2/agendamento_consultas/AgendamentoConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package lp2.agendamento_consultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoConsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
